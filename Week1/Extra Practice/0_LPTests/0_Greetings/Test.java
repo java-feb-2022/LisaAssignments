1365a4b445ee4485e7618c1eab9a5c485cef8e79
Lisa <lisa.hl.chen@gmail.com>
2022-02-06 17:06:40 -0800

    FirstJavaProgram

---
+++ Week1/Extra Practice/0_LPTests/0_Greetings/Test.java
@@ -0,0 +1,16 @@
+// the class has to be the file name
+public class Test {
+    public static void main(String[] args) {
+        // public static void = func
+        // main = name of the method
+        // Each parameter lists what kind of data it will be
+        // The file or class with the exact method signature below
+        // is always the launching point of the application
+        // You cannot run a Java program without it.
+        System.out.println("Hello");
+    }
+}
+
+
+// now run javac Test.java
+// now run java Test (.class)
---
+++ Week1/Extra Practice/1_FirstJavaProgram/Daffy.java
@@ -0,0 +1,7 @@
+public class Daffy {
+    public static void main(String[] args) {
+        System.out.println("My name is Daffy Duck");
+        System.out.println("You don't need to know my age");
+        System.out.println("My hometown is NOT Burbank, CA");
+    }
+}
