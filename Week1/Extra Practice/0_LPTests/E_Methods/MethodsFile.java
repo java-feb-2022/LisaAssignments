7a1dcb7f5ecdc1ba601069c28769481ba1559af2
Lisa <lisa.hl.chen@gmail.com>
2022-02-07 22:32:32 -0800

    updated methods example file

--- Week1/Extra Practice/0_LPTests/E_Methods/MethodsFile.java
+++ Week1/Extra Practice/0_LPTests/E_Methods/MethodsFile.java
@@ -1,16 +1,33 @@
 public class MethodsFile {
     
     //void gives you permission to not return anything
-    // Signature
+    // Signature (every method's signature of guestGreeting(String, String))
+        //Name of method + parameter types
+        
     // name of function and parameters of function (greeter (String name))
     public String greeter(String name, Date date) {
         return "Hello World!";
     }
 
+    public void coolMethodName(int someNumber, String someString) {
+        System.out.println("Hello World");
+    }   
+
     public String greeter(String name) {
         return "Hello World!";
     }
 
+    // METHOD OVERLOADING - same function, different parameters
+    // ...
+    public String guestGreeting(String name) {
+        return ("Hello World!");
+    }
+
+    // ...
+    public String guestGreeting(String name, String dayPeriod) {
+        return ("Hello World!");
+    }
+    // ...
 
 
 }
---
+++ Week1/Extra Practice/0_LPTests/F_Modularization/AwesomeAppStarter.java
@@ -0,0 +1,7 @@
+public class AwesomeAppStarter {
+    public static void main(String[] args) {
+        //some code to start app or
+        // import and test code from other files
+        
+    }
+}
