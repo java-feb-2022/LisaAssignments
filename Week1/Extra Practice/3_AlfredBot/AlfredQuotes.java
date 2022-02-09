3c2cd26d8ea153f202f1beaa2873f1a28d2746d6
Lisa <lisa.hl.chen@gmail.com>
2022-02-08 21:25:53 -0800

    updates

--- Week1/Extra Practice/3_AlfredBot/AlfredQuotes.java
+++ Week1/Extra Practice/3_AlfredBot/AlfredQuotes.java
@@ -1,25 +1,35 @@
 import java.util.Date;
+import java.util.Calendar;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 public class AlfredQuotes {
 
     public String basicGreeting() {
         // You do not need to code here, this is an example method
-        return "Hello, lovely to see you. How are you?";
+        return "Hey, what's up? I am Alfred-o";
     }
 
     public String guestGreeting(String name) {
-        // YOUR CODE HERE
-        return "place holder for guest greeting return string";
+        String guestGreet = String.format("Hello %s, are you having loads of fun? Because I am!", name);
+        return guestGreet;
     }
 
     public String dateAnnouncement() {
-        // YOUR CODE HERE
-        return "place holder for date announcement return string";
+        Date today = Calendar.getInstance().getTime();
+        String announcement = String.format("Hello! Today is %s.", today); 
+        return announcement;
     }
 
     public String respondBeforeAlexis(String conversation) {
-        // YOUR CODE HERE
-        return "for snarky response return string";
+        
+        if (conversation.indexOf("Alexis") > -1) {
+            return "How do you get Pikachu on the bus? You pok-em-on!";
+        }
+        if (conversation.indexOf("Alfred") > -1) {
+            return "Where's your yo-yo? No, suck it";
+        }
+        return "Well, never mind";
     }
 
     // NINJA BONUS
