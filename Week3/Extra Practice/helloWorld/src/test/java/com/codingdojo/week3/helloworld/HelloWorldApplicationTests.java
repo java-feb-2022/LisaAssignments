2b863176e6f59d53ddd61de50a9796a23f26ec36
Lisa <lisa.hl.chen@gmail.com>
2022-02-21 21:45:11 -0800

    update

---
+++ Week3/Extra Practice/helloWorld/src/test/java/com/codingdojo/week3/helloworld/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.week3.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
