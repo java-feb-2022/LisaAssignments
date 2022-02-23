2622dd247096c5d78f11ab0226e7d45fb0bd6fc1
Lisa <lisa.hl.chen@gmail.com>
2022-02-22 21:19:50 -0800

    update

--- Week3/Extra Practice/controllersPractice/src/main/java/com/codingdojo/week3/controllersPractice/HomeController.java
+++ Week3/Extra Practice/controllersPractice/src/main/java/com/codingdojo/week3/controllersPractice/HomeController.java
@@ -2,11 +2,24 @@ package com.codingdojo.week3.controllersPractice;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 @RestController
+@RequestMapping("/greeting")
 
 public class HomeController {
-    @RequestMapping("/")
-    public String hello() {
-            return "Hello World!";
-    }
+	@RequestMapping("")
+	public String index(){
+		return "Hello user!";
+	}
+	// you can be explicit about the request as well
+ 	@RequestMapping(value="/hello", method=RequestMethod.GET)
+ 	public String hello(){
+		return "Hello world! What route did you use to access me?";
+ 	}
+ 	@RequestMapping("/goodbye")
+ 	public String world(){
+ 		return "Goodbye world!";
+ 	}
 }
+
