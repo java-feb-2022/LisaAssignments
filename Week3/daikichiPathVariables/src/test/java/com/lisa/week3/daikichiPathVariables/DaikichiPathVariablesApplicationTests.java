261ad2bfc4b73a6bb11f8c5602c6d43195c1bcf4
Lisa <lisa.hl.chen@gmail.com>
2022-02-23 23:06:25 -0800

    update

---
+++ Week3/daikichiPathVariables/src/test/java/com/lisa/week3/daikichiPathVariables/DaikichiPathVariablesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lisa.week3.daikichiPathVariables;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaikichiPathVariablesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
