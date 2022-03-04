5cbe81790957adf7b1b43193bfe22659eb3843d3
Lisa <lisa.hl.chen@gmail.com>
2022-03-04 00:27:09 -0800

    update

---
+++ Week4/Extra Practice/W4_B_DataRepository/src/main/java/com/lisa/datarepositories/mvc/repositories/BookRepository.java
@@ -0,0 +1,24 @@
+package com.lisa.datarepositories.mvc.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lisa.datarepository.models.Book;
+
+@Repository // manages dependencies for us
+public interface BookRepository extends CrudRepository<Book, Long>{
+	
+    // this method retrieves all the books from the database
+    List<Book> findAll();
+    
+    // this method finds books with descriptions containing the search string
+    List<Book> findByDescriptionContaining(String search);
+    
+    // this method counts how many titles contain a certain string
+    Long countByTitleContaining(String search);
+    
+    // this method deletes a book that starts with a specific title
+    Long deleteByTitleStartingWith(String search);
+}
